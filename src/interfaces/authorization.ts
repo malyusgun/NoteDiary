5f2e207e2af99847ca119f36b9dffc240a367c76
d.malygin <d.malygin@iqdev.digital>
2024-08-07 10:39:41 +0500

    create base menu

--- src/interfaces/authorization.ts
+++ src/interfaces/authorization.ts
@@ -2,7 +2,10 @@ import type { IImage, TThemes } from '@/interfaces/environment';
 
 export interface IUserData {
   uuid: string;
-  fullName: string;
+  nickName: string;
+  firstName: string;
+  lastName: string;
+  middleName: string;
   email: string;
   phoneNumber: string;
   settings: IUserSettings;
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from './router';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
-app.use(PrimeVue);
-app.mount('#app');
+app
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {}
+    }
+  })
+  .use(createPinia())
+  .use(router)
+  .mount('#app');
